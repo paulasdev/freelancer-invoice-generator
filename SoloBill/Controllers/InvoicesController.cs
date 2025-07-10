16db8e0dfed90732ef4ea6affd79564aeb622f5a
Paula Hickey <carolpaulag@gmail.com>
2025-07-10 20:53:36 +0100

    Implemented full CRUD for Clients and Invoices with SQL Server integration

---
+++ SoloBill/Controllers/InvoicesController.cs
@@ -0,0 +1,163 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SoloBill.Data;
+
+namespace SoloBill.Controllers
+{
+    public class InvoicesController : Controller
+    {
+        private readonly SoloBillDbContext _context;
+
+        public InvoicesController(SoloBillDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Invoices
+        public async Task<IActionResult> Index()
+        {
+            var soloBillDbContext = _context.Invoices.Include(i => i.Client);
+            return View(await soloBillDbContext.ToListAsync());
+        }
+
+        // GET: Invoices/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var invoice = await _context.Invoices
+                .Include(i => i.Client)
+                .FirstOrDefaultAsync(m => m.InvoiceId == id);
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+
+            return View(invoice);
+        }
+
+        // GET: Invoices/Create
+        public IActionResult Create()
+        {
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "ClientId");
+            return View();
+        }
+
+        // POST: Invoices/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("InvoiceId,ClientId,IssueDate,DueDate,Amount,IsPaid")] Invoice invoice)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(invoice);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "ClientId", invoice.ClientId);
+            return View(invoice);
+        }
+
+        // GET: Invoices/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var invoice = await _context.Invoices.FindAsync(id);
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "ClientId", invoice.ClientId);
+            return View(invoice);
+        }
+
+        // POST: Invoices/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("InvoiceId,ClientId,IssueDate,DueDate,Amount,IsPaid")] Invoice invoice)
+        {
+            if (id != invoice.InvoiceId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(invoice);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InvoiceExists(invoice.InvoiceId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "ClientId", invoice.ClientId);
+            return View(invoice);
+        }
+
+        // GET: Invoices/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var invoice = await _context.Invoices
+                .Include(i => i.Client)
+                .FirstOrDefaultAsync(m => m.InvoiceId == id);
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+
+            return View(invoice);
+        }
+
+        // POST: Invoices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var invoice = await _context.Invoices.FindAsync(id);
+            if (invoice != null)
+            {
+                _context.Invoices.Remove(invoice);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InvoiceExists(int id)
+        {
+            return _context.Invoices.Any(e => e.InvoiceId == id);
+        }
+    }
+}
---
+++ SoloBill/Data/SoloBillDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace SoloBill.Data;
+
+public class SoloBillDbContext : IdentityDbContext<IdentityUser>
+{
+    public SoloBillDbContext(DbContextOptions<SoloBillDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Invoice> Invoices { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        Prevents truncation of decimal values in SQL Server
+        modelBuilder.Entity<Invoice>()
+            .Property(i => i.Amount)
+            .HasPrecision(18, 2);
+
+//Test        
+modelBuilder.Entity<Client>().HasData(new Client
+{
+    ClientId = 1,
+    Name = "ACME Inc.",
+    Email = "info@acme.com",
+    Company = "ACME Inc.",
+    Address = "123 Main Street"
+});
+
+modelBuilder.Entity<Invoice>().HasData(new Invoice
+{
+    InvoiceId = 1,
+    ClientId = 1,
+    IssueDate = new DateTime(2025, 7, 1),
+    DueDate = new DateTime(2025, 7, 31),
+    Amount = 500.00m,
+    IsPaid = false
+});
+    }
+}
