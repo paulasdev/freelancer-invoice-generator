f92265777d79892472526b013d3466ec2f88e15e
Paula Hickey <carolpaulag@gmail.com>
2025-07-14 18:25:09 +0100

    Configure services and set global culture to en-IE

---
+++ SoloBill/Migrations/20250714165355_InitialIdentitySetup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoloBill.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialIdentitySetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompanyName",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CompanyName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
