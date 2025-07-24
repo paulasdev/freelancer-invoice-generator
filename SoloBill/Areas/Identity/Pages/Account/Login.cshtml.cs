03585f52dbf5d6ede06362d84d80cf6b29347abb
Paula Hickey <carolpaulag@gmail.com>
2025-07-24 23:18:38 +0100

    implement custom login and registration

--- SoloBill/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ SoloBill/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -104,14 +104,13 @@ namespace SoloBill.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Dashboard");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
             if (ModelState.IsValid)
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
@@ -134,7 +133,6 @@ namespace SoloBill.Areas.Identity.Pages.Account
                 }
             }
 
-            // If we got this far, something failed, redisplay form
             return Page();
         }
     }
