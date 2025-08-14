2a51ac197955fff606c7e09b1e81fae8095290c6
Paula Hickey <carolpaulag@gmail.com>
2025-08-14 16:04:11 +0100

    Updated page layout and CSS

---
+++ SoloBill/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SoloBill.Models;
+
+namespace SoloBill.Areas.Identity.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public LogoutModel(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToPage("/Account/Login"); // 👈 redirect after logout
+        }
+
+        public async Task<IActionResult> OnPost(string returnUrl = null)
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToPage("/Account/Login"); // 👈 also redirect after form POST logout
+        }
+    }
+}
