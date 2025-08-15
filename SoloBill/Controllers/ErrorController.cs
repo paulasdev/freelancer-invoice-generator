bc81711b7129ba27892dea28e54ac893d0c44d35
Paula Hickey <carolpaulag@gmail.com>
2025-08-15 15:10:52 +0100

    Fix error views: remove Response usage; add ErrorController to set status codes and route 404/500

--- SoloBill/Controllers/ErrorController.cs
+++ SoloBill/Controllers/ErrorController.cs
@@ -1,14 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 
-[Route("Error")]
 public class ErrorController : Controller
 {
-    // Handles /Error/404, /Error/401, etc.
-    [Route("{statusCode:int}")]
+    [Route("Error/{statusCode}")]
     public IActionResult HttpStatusCodeHandler(int statusCode)
-        => statusCode == 404 ? View("Error404") : View("Error");
+    {
+        Response.StatusCode = statusCode;
+        return statusCode == 404 ? View("Error404") : View("Error");
+    }
 
-    // Handles /Error/500 from UseExceptionHandler
-    [Route("500")]
-    public IActionResult Error500() => View("Error500");
+    [Route("Error/500")]
+    public IActionResult Error500()
+    {
+        Response.StatusCode = 500;
+        return View("Error500");
+    }
 }
